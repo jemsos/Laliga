57ad29c824a5df55222ac4c7f335b775e484a7a1
Jemson Sayre <jemsos@bss-ph.com>
2021-12-21 14:57:56 +0800

    sub01 laliga-pattern apply the slick carousel and modify the design

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
 
+    /**Slick */
+    $('.laliga-slider').slick({
+        autoplay: true,
+        dots: true,
+        // arrows: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        autoplaySpeed: 5000,
+        prevArrow: '<div class="arrow prev">\
+        <div class="arrow-bottom"></div>\
+        </div>',
+        nextArrow: '<div class="arrow next">\
+        <div class="arrow-top"></div>\
+        </div>',
+    });
+
 });
