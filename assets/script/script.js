d40dc2d0694c03a6bf8adf65378b05431f1cc93b
Jemson Sayre <jemsos@bss-ph.com>
2022-01-06 11:39:36 +0800

    sub01 replace slick slider with swiper carousel on pattern section

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,41 +1,40 @@
 $(document).ready(function(){
 
-    /**Slick */
-    $('.laliga-slider').slick({
-        autoplay: true,
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 4,
-        autoplaySpeed: 5000,
-        prevArrow: '<div class="arrow prev">\
-        <div class="arrow-bottom"></div>\
-        </div>',
-        nextArrow: '<div class="arrow next">\
-        <div class="arrow-top"></div>\
-        </div>',
-        responsive: [{
-            breakpoint: 1366,
-            settings:{
-                slidesToShow: 3,
-            }
-        },{
-            breakpoint: 1024,
-            settings:{
-                slidesToShow: 3,
-                dots: false,
-                arrows: false,
-            }
-        },
-        {
-            breakpoint: 767,
-            settings:{
-                slidesToShow: 1,
-                dots: false,
-                arrows: false,
+    /**Swipe */
+    const sliderSwiper = new Swiper('.swiper-pattern',{
+        slidesPerView: 1,
+        spaceBetween: 10,
+        navigation: false,
+        breakpoints: {
+            340: {
+                slidesPerView: 1.5,
+                spaceBetween: 10,
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                  },
+            },
+            640: {
+                slidesPerView: 1.8,
+                spaceBetween: 20,
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                  },
+            },
+            767: {
+                slidesPerView: 1.5,
+                spaceBetween: 30,
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                  },
+            },
+            1024: {
+                slidesPerView: 4,
+                spaceBetween: 40,
+                navigation: false,
             }
         }
-        ]
-    });
-
+    })
 });
