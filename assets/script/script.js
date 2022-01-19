ef338e3de4abd27f4a43b84e8216bc8a6161041c
Joey <joeybo@bss-ph.com>
2022-01-19 08:32:22 +0800

    added laliga more sports

--- assets/script/script.js
+++ assets/script/script.js
@@ -39,8 +39,6 @@ $(document).ready(function(){
             }
         }
     });
-
-
    
     const sliderSwiper2 = new Swiper('.swiper-laliga-room',{
         navigation: false,
@@ -65,7 +63,53 @@ $(document).ready(function(){
             }
         }
     });
-    
+
+    function toggleSwiper(){
+        const sliderSwiper3 = new Swiper('.swiper-more',{
+            navigation: false,
+            breakpoints: {
+                340: {
+                    slidesPerView: 1.1
+                },
+                480: {
+                    slidesPerView: 1.4
+                },
+                560: {
+                    slidesPerView: 1.6
+                },
+                640: {
+                    slidesPerView: 1.9
+                },
+                767: {
+                    slidesPerView: 2.25
+                },
+                960: {
+                    slidesPerView: 2.7
+                },
+                1023: {
+                    slidesPerView: 3
+                }
+            }
+        });   
+        if ( $(window).width() < 1024 ) {   
+            $( ".laliga-more-sports .container > .swiper > div" ).addClass( "swiper-wrapper" );
+            $( ".laliga-more-sports .container > .swiper > div > div" ).addClass( "swiper-slide" );  
+            sliderSwiper3.init(); 
+        } else {
+            $( ".laliga-more-sports .container > .swiper > div" ).removeClass( "swiper-wrapper" );
+            $( ".laliga-more-sports .container > .swiper > div > div" ).removeClass( "swiper-slide" );   
+            sliderSwiper3.destroy();
+        }
+    }
+    toggleSwiper();
+    // // on load
+    // $(window).load(function() {
+    //     toggleSwiper();
+    // });
+    // on resize
+    $(window).resize(function() {
+        toggleSwiper();
+    });
 
     
 });
