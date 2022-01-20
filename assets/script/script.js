9caab3f8a9f93a8424eabffe5ca0fbaa32b1f5e1
Joey <joeybo@bss-ph.com>
2022-01-20 15:24:03 +0800

    odds navs adjstmnt

--- assets/script/script.js
+++ assets/script/script.js
@@ -67,6 +67,9 @@ $(document).ready(function(){
                     nextEl: '.swiper-button-next',
                     prevEl: '.swiper-button-prev',
                 },
+                pagination: {
+                    el: ".swiper-pagination",
+                },
             }
         }
     });
