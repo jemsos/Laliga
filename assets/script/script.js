7118a2746e638ee7d3e92a2fccb907e94773bb82
Jemson Sayre <jemsos@bss-ph.com>
2022-01-27 11:21:30 +0800

    sub01 mgoal api

--- assets/script/script.js
+++ assets/script/script.js
@@ -145,6 +145,123 @@ $(document).ready(function(){
 
     $(window).resize(function() {
         toggleSwiper();
+        setSwiperDta();
     });
+
+    //odds
+    let apiURL = "https://api.mfeeduno.com/api";
+    let imageURL = apiURL + "/mgoalfeed/getteamicon?filename=";
+
+    let selectedMatchCat = 1;
+    let refreshMatches = null;
+
+    function loadMatches(){
+        let selectedMS = 1;
+        let endpoint = apiURL + "/mgoalfeed/";
+
+        if(selectedMS == undefined || selectedMS == 1){
+            endpoint += "gettodaymatches";
+        }
+
+        $.get(endpoint, function(data) {
+            console.log(data)
+            if(data != null && data.length > 0){
+                oddMatch(data);
+            }else{
+                $(".laliga-slider").html(""); 
+            }
+
+            refreshMatches = setInterval(function(){
+                // console.log(refreshMatches)
+                loadMatches(selectedMatchCat);
+            }, 30000);
+        });
+    };
+
+    function setSwiperDta(w){
+        let patternW = $('.swiper.swiper-pattern').width();
+        let slidesPerView;
+        let slidesPerBetween;
+        let newWidth;
+        if(patternW <= '340' && patternW == '340'){
+            slidesPerView = 1.25;
+            slidesPerBetween = 10;
+        }else if(patternW >= "640" && patternW <= '766'){
+            slidesPerView = 1.25;
+            slidesPerBetween = 10;
+
+        }else if(patternW >= "640" && patternW <= '766'){
+            slidesPerView = 3.24;
+            slidesPerBetween = 10;
+
+        }else if(patternW >= "767" && patternW <= '1023'){
+            slidesPerView = 2.5;
+            slidesPerBetween =10;
+        }else {
+            slidesPerView = 3.24;
+            slidesPerBetween = 25;
+            
+        }
+        newWidth = patternW / Math.floor(slidesPerView);
+        newWidth = Math.round(newWidth)
+        console.log(newWidth)
+        $('.swiper-slide').css({'width':newWidth, 'margin-right': slidesPerBetween + 'px' })
+    }
+
+    //date
+    function formatDate(date){
+        let d = new Date();
+        let newDate = d.toLocaleString('en-us', { month: 'short', day: 'numeric', });
+        return newDate;
+    }
+
+    function oddMatch(data){
+        let template =  '<div class="swiper-slide" >\
+            <div class="card figure-game">\
+                <div class="card-body">\
+                    <div class="laliga-crest-heading">\
+                        <img src="assets/img/laliga-pattern/LALIGA.png" alt="" srcset="">\
+                        <span class="separator"></span>\
+                        <span class="laliga-date">[DATE]</span>\
+                    </div>\
+                    <div class="laliga-crest-content">\
+                        <div class="laliga-matches">\
+                            <p>[TEAM1]</p>\
+                            <p>[TEAM2]</p>\
+                        </div>\
+                        <div class="laliga-crest">\
+                            <img src="[HOMETEAMICON]" alt="" srcset="">\
+                            <img src="[AWAYTEAMICON]" alt="" srcset="">\
+                        </div>\
+                    </div>\
+                </div>\
+            </div>\
+        </div>';
+
+        let html = '';
+        let newCount;
+        for(let i = 0; i < data.length; i++){
+            newCount = i;
+            let homeIndicator = "";
+            let awayIndicator = "";
+
+            html += template.replace("[LEAGUE]", data[i].tournament)
+                    .replace("[TEAM1]", data[i].homeTeam)
+                    .replace("[TEAM2]", data[i].awayTeam)
+                    .replace("[HOMEODDS]", data[i].homeOdds)
+                    .replace("[AWAYODDS]", data[i].awayOdds)
+                    .replace("[DRAWODDS]", data[i].drawOdds)
+                    .replace("[HOMETEAMICON]", imageURL + data[i].homeTeamIcon)
+                    .replace("[AWAYTEAMICON]", imageURL + data[i].awayTeamIcon)
+                    .replace("[HOMEINDICATOR]", homeIndicator)
+                    .replace("[AWAYINDICATOR]", awayIndicator)
+                    .replace("[DATE]", formatDate(data[i].date))
+                    .replace("[INDEX]", i);
+        }
+
+        $(".laliga-slider").html(html);
+    }
+
+    loadMatches();
 });
 
